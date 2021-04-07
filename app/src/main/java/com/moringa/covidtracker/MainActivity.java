f0afbb57fe5b4716ad64a4f38b16073c93af1020
BarakaIan <ianb.makale@gmail.com>
2021-04-07 14:51:39 +0300

    add some changes to the manifest files

--- app/src/main/java/com/moringa/covidtracker/MainActivity.java
+++ app/src/main/java/com/moringa/covidtracker/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+
         //Onclick function for navigating to the next activity
         mHomePageEnterButton.setOnClickListener(new View.OnClickListener() {
             @Override

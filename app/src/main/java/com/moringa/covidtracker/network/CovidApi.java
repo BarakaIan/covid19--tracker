ba581a426e64fe0168a525bfa0bb4a4d0b4db276
BarakaIan <ianb.makale@gmail.com>
2021-04-07 09:36:07 +0300

    add the necessary changes

---
+++ app/src/main/java/com/moringa/covidtracker/network/CovidApi.java
@@ -0,0 +1,18 @@
+package com.moringa.covidtracker.network;
+
+import com.moringa.covidtracker.models.CovidCases;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface CovidApi {
+    @GET("cases")
+    Call<CovidCases> getCases(
+//            @Query("continent") String continent,
+            @Query("country") String country
+    );
+
+}

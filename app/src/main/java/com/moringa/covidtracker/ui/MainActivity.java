fe3a20035819af28d3630c1f30b2063875059da2
BarakaIan <ianb.makale@gmail.com>
2021-04-09 09:31:16 +0300

    add some UI stylings

--- app/src/main/java/com/moringa/covidtracker/ui/MainActivity.java
+++ app/src/main/java/com/moringa/covidtracker/ui/MainActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,63 +17,75 @@ import com.moringa.covidtracker.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.HomepageEnterButton) Button mHomePageEnterButton;
+    @BindView(R.id.nameEditText)
+    EditText mNameEditText;
 
-    @BindView(R.id.HomepageEnterButton)
-    Button mAboutButton;
-    @BindView(R.id.appNameTextView)
-    TextView mappNameTextView;
+    String country;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        }
 
-    //adding the additional menu to the menu bar
+        //Onclick listener for the enter button and passing country input to the country activity
+        mHomePageEnterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                country = mNameEditText.getText().toString();
+                country = toJadenCase(country);
+                Intent intent = new Intent(MainActivity.this,NewAccountActivity.class);
+                intent.putExtra("country", country);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //Create the additional menu with the button
     @Override
-    public boolean onCreateOptionsMenu (Menu
-    menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_bar, menu);
         return true;
     }
 
-    //setting up functions for the menu bar items
-//    @Override
-//    public boolean onOptionsItemSelected (MenuItem item){
-//        int id = item.getItemId();
-//        if (id == R.id.action_logout) {
-//            logout();
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//
-//        if (item.getItemId() == R.id.item1) {
-//            Intent intent = new Intent(MainActivity.this, CountryActivity.class);
-//            startActivity(intent);
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-
-
-
-
+    //Create functions to be performed on the items in the menu
     @Override
-    public void onClick(View v) {
-        mAboutButton.setOnClickListener(this);
-        if (v == mAboutButton) {
-            String name = mappNameTextView.getText().toString();
-            Intent intent = new Intent(MainActivity.this, CountryActivity.class);
-            intent.putExtra("userName", name);
-            startActivity(intent);
-
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                country = mNameEditText.getText().toString();
+                country = toJadenCase(country);
+                Intent intent = new Intent(MainActivity.this, FAQActivity.class);
+                intent.putExtra("country", country);
+                startActivity(intent);
+                return true;
+            case R.id.action_logout:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
+    //For converting to JadenCase which is the form that the api takes for the country input e.g: sierra leone = Sierra Leone
+    public String toJadenCase(String phrase) {
+        if(phrase == null || phrase.equals("")) return null;
+
+        char[] array = phrase.toCharArray();
+
+        for(int x = 0; x < array.length; x++) {
+            if(x == 0 || array[x-1] == ' ') {
+                array[x] = Character.toUpperCase(array[x]);
+            }
+        }
+
+        return new String(array);
+    }
+
     private void logout() {
-        FirebaseAuth.getInstance().signOut();
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);

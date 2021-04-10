6e494707c962d7cbab0868fdc0e76779e02046cd
BarakaIan <ianb.makale@gmail.com>
2021-04-10 13:22:05 +0300

    change the API and consumed it

--- app/src/main/java/com/moringa/covidtracker/network/CoronaApi.java
+++ app/src/main/java/com/moringa/covidtracker/network/CoronaApi.java
@@ -1,46 +1,21 @@
 package com.moringa.covidtracker.network;
 
-import com.moringa.covidtracker.models.AllCountriesResponse;
-import com.moringa.covidtracker.models.CountriesResponse;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-import java.util.List;
+public class CoronaApi {
 
-import retrofit2.Call;
+    private static Retrofit retrofit;
+    private static final String CORONA_BASE_URL = "https://corona.lmao.ninja/v2/";
 
-import retrofit2.http.GET;
-import retrofit2.http.Path;
+    public static Retrofit getRetrofitInstance() {
 
-
-public interface CoronaApi {
-
-
-    String CORONA_BASE_URL = null;
-
-    static void getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(CORONA_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
     }
-
-
-    @GET("countries/?sort=country")
-    Call<List<CountriesResponse>> getCountries();
-
-
-    @GET("countries/{country}")
-    Call<CountriesResponse> getCountryInfo(
-
-            @Path("country") String country
-    );
-
-    @GET("all")
-    Call<AllCountriesResponse> getAllCountries();
-
-//    static Retrofit getRetrofitInstance() {
-//
-//        if (retrofit == null) {
-//            retrofit = new retrofit2.Retrofit.Builder()
-//                    .baseUrl(CORONA_BASE_URL)
-//                    .addConverterFactory(GsonConverterFactory.create())
-//                    .build();
-//        }
-//        return retrofit;
-//    }
 }
